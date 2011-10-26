8a7d3608416c67c0106a25190b1f4ebf51a7720c
faux123 <reioux@gmail.com>
2011-10-26 07:48:18 -0700

    kgsl: GPU slumber code update (part 1)

--- drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl.c
+++ drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl.c
@@ -44,6 +44,7 @@
 #include "kgsl_drm.h"
 #include "kgsl_cffdump.h"
 
+static bool screen_off = false;
 
 static void kgsl_put_phys_file(struct file *file);
 
@@ -304,7 +305,6 @@ static int kgsl_suspend(struct platform_device *dev, pm_message_t state)
 			break;
 		case KGSL_STATE_SLUMBER:
 			INIT_COMPLETION(device->hwaccess_gate);
-			device->state = KGSL_STATE_SUSPEND;
 			KGSL_PWR_INFO("state -> SUSPEND, device %d\n",
 				device->id);
 			break;
@@ -314,6 +314,7 @@ static int kgsl_suspend(struct platform_device *dev, pm_message_t state)
 			mutex_unlock(&device->mutex);
 			return KGSL_FAILURE;
 		}
+		device->state = KGSL_STATE_SUSPEND;
 		device->requested_state = KGSL_STATE_NONE;
 		device->pwrctrl.nap_allowed = nap_allowed_saved;
 		device->pwrctrl.idle_pass = idle_pass_saved;
@@ -338,14 +339,31 @@ static int kgsl_resume(struct platform_device *dev)
 			continue;
 
 		mutex_lock(&device->mutex);
-		if (device->state == KGSL_STATE_SUSPEND) {
+		if ((device->state == KGSL_STATE_SUSPEND) &&
+			screen_off) {
 
 			device->state = KGSL_STATE_SLUMBER;
 			status = 0;
 			KGSL_PWR_INFO("state -> SLUMBER, device %d\n",
 					device->id);
-			complete_all(&device->hwaccess_gate);
+		} else if (device->state == KGSL_STATE_SUSPEND) {
+			device->requested_state = KGSL_STATE_ACTIVE;
+			kgsl_pwrctrl_pwrlevel_change(device, KGSL_PWRLEVEL_NOMINAL);
+			status = device->ftbl.device_start(device, 0);
+			if (status == KGSL_SUCCESS) {
+				device->state = KGSL_STATE_ACTIVE;
+				KGSL_PWR_WARN("state -> ACTIVE, device %d\n",
+						device->id);
+			} else {
+				KGSL_PWR_ERR("resume failed, device %d\n",
+						device->id);
+				device->state = KGSL_STATE_INIT;
+				mutex_unlock(&device->mutex);
+				return status;
+			}
+			status = device->ftbl.device_resume_context(device);
 		}
+		complete_all(&device->hwaccess_gate);
 		device->requested_state = KGSL_STATE_NONE;
 		mutex_unlock(&device->mutex);
 	}
@@ -360,6 +378,7 @@ void kgsl_early_suspend_driver(struct early_suspend *h)
 
 	KGSL_PWR_INFO("early suspend start\n");
 	mutex_lock(&device->mutex);
+	screen_off = true;
 	device->requested_state = KGSL_STATE_SLUMBER;
 	kgsl_pwrctrl_sleep(device);
 	mutex_unlock(&device->mutex);
@@ -374,6 +393,7 @@ void kgsl_late_resume_driver(struct early_suspend *h)
 
 	KGSL_PWR_INFO("late resume start\n");
 	mutex_lock(&device->mutex);
+	screen_off = false;
 	kgsl_pwrctrl_wake(device);
 	device->pwrctrl.restore_slumber = 0;
 	mutex_unlock(&device->mutex);
